f69cf8e2c88413006a70da2ae036407fefa0ab85
srel <srel@SREL-IN.in.oracle.com>
2019-11-18 14:38:51 +0530

    2nd commit

---
+++ src/main/java/com/example/demo/HelloAppController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloAppController {
+	
+	
+	@RequestMapping("/hi")
+	public String hello()
+	{
+		return "HI";
+	}
+	
+
+}
